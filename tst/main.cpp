960e7e20118e01d8b27dd27829a77275330ae051
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-11-15 00:15:47 -0600

    Test

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,6 +1,11 @@
 //
 // Created by Tere Solano on 14/11/2020.
 //
+#include "gtest/gtest.h"
+
 int main(){
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
     return 0;
 }
